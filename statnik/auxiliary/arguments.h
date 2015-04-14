d081009f45875ca497fcbcfae6e576df79364153
Evgeny Statnik <statnikeugeny@gmail.com>
2015-04-14 20:28:15 +0300

    Another tiny change in argument parser

--- statnik/auxiliary/arguments.h
+++ statnik/auxiliary/arguments.h
@@ -34,7 +34,7 @@ class Arguments {
     bool check() {
         for (Argument& argument : arguments) {
             if (!argument.used) {
-                if (argument.name != "help" && argument.name == "h" && argument.name == "?") {
+                if (argument.name != "help" && argument.name != "h" && argument.name != "?") {
                     std::cerr << "Argument " << argument.name << " does not exists" << std::endl;
                 }
                 return false;
